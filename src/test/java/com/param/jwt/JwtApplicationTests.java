8a464d57872b20e728e42ffc1b5662f7c70e3849
paramwaseel <panarapa@waseel.com>
2023-10-28 11:53:21 +0530

    Initial Commit

---
+++ src/test/java/com/param/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.param.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
