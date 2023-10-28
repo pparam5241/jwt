85835fbb3c6e46956691528c47baaba51eafe9d4
paramwaseel <panarapa@waseel.com>
2023-10-28 12:58:26 +0530

    JWT Token Generation with InMemory User

--- src/main/java/com/param/jwt/configuration/WebSecurity.java
+++ src/main/java/com/param/jwt/configuration/WebSecurity.java
@@ -1,21 +1,33 @@
 package com.param.jwt.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.param.jwt.filter.JwtAuthenticationFilter;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtAuthenticationEntryPoint point;
+
+	@Autowired
+	private JwtAuthenticationFilter filter;
 
 	@Bean
 	public UserDetailsService userDetailsService() {
@@ -32,5 +44,14 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
 		return builder.getAuthenticationManager();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()).authorizeRequests().antMatchers("/test").authenticated()
+				.antMatchers("/auth/login").permitAll().anyRequest().authenticated().and()
+				.exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
---
+++ src/main/java/com/param/jwt/models/JwtRequest.java
@@ -0,0 +1,37 @@
+package com.param.jwt.models;
+
+public class JwtRequest {
+	private String userName;
+	private String password;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public JwtRequest(String userName, String password) {
+		super();
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public JwtRequest() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "JwtRequest [userName=" + userName + ", password=" + password + "]";
+	}
+}
